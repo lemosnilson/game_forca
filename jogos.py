f053f9e9ed8acac6442d4dd5b73e4b31ad12f911
Nilson Lemos <128808216+lemosnilson@users.noreply.github.com>
2023-07-12 16:42:44 -0300

    final update

---
+++ jogos.py
@@ -0,0 +1,24 @@
+import forca
+import adivinhacao
+
+
+def escolhe_jogo():
+
+    print("*********************************")
+    print("*******Escolha o seu Jogo********")
+    print("*********************************")
+
+    print("(1) Forca  (2) Adivinhação")
+
+    jogo = int(input("Qual jogo?"))
+
+    if jogo == 1:
+        print("jogando forca")
+        forca.jogar()
+    elif jogo == 2:
+        print("jogando adivinhação")
+        adivinhacao.jogar()
+
+
+if __name__ == "__main__":
+    escolhe_jogo()
